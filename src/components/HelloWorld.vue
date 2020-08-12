9a7a464408cc30e21fd3625a8a16d79c73eb56ea
reidefe <reidefe@gmail.com>
2020-08-12 13:10:22 +0300

    modified:   public/index.html 	modified:   src/App.vue 	new file:   src/assets/logo.svg 	new file:   src/cars-1.json 	modified:   src/components/HelloWorld.vue 	new file:   src/data/cars-1.json 	new file:   src/data/cars-2.json 	new file:   src/data/cars-3.json 	new file:   src/data/cars-4.json 	new file:   src/data/cars-5.json 	modified:   src/main.js 	new file:   src/plugins/vuetify.js 	new file:   vue.config.js

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,46 +1,254 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+ 
+      <v-data-table 
+       :headers="tableItems"
+        :items='allCars'
+        :items-per-page='15'
+        class="elevation-1">       
+        
+      </v-data-table>
+      <!--      
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Miles_per_Gallon</th>
+          <th>Cylinders</th>
+          <th>Displacement</th>
+          <th>Horsepower</th>
+          <th>Weight_in_lbs</th>
+          <th>Acceleration</th>
+          <th>Year</th>
+          <th>Origin</th>
+        </tr>
+        <tr  v-for="car in allCars" v-bind:key="car">
+          <th> 
+            <td>{{car.Name}}</td>
+          </th>
+          <th>
+            <td>{{car.Miles_per_Gallon}}</td>
+          </th>
+          <th>
+            <td>{{car.Cylinders}}</td>
+          </th>
+          <th>
+            <td>{{car.Displacement}}</td>
+
+          </th>
+          <th>
+            <td>{{car.Horsepower}}</td>
+          </th>
+          <th>
+             <td>{{car.Weight_in_lbs}}</td>
+          </th>
+          <th>
+            <td>{{car.Acceleration}}</td>
+          </th>
+          <th>
+            <td>{{car.Year}}</td>
+          </th>
+          <th>
+            <td>{{car.Origin}}</td>
+          </th>
+        </tr>
+         <tr  v-for="car in carsJson2" v-bind:key="car">
+          <th> 
+            <td>{{car.Name}}</td>
+          </th>
+          <th>
+            <td>{{car.Miles_per_Gallon}}</td>
+          </th>
+          <th>
+            <td>{{car.Cylinders}}</td>
+          </th>
+          <th>
+            <td>{{car.Displacement}}</td>
+
+          </th>
+          <th>
+            <td>{{car.Horsepower}}</td>
+          </th>
+          <th>
+             <td>{{car.Weight_in_lbs}}</td>
+          </th>
+          <th>
+            <td>{{car.Acceleration}}</td>
+          </th>
+          <th>
+            <td>{{car.Year}}</td>
+          </th>
+          <th>
+            <td>{{car.Origin}}</td>
+          </th>
+        </tr>  
+         <tr  v-for="car in carsJson3" v-bind:key="car">
+          <th> 
+            <td>{{car.Name}}</td>
+          </th>
+          <th>
+            <td>{{car.Miles_per_Gallon}}</td>
+          </th>
+          <th>
+            <td>{{car.Cylinders}}</td>
+          </th>
+          <th>
+            <td>{{car.Displacement}}</td>
+
+          </th>
+          <th>
+            <td>{{car.Horsepower}}</td>
+          </th>
+          <th>
+             <td>{{car.Weight_in_lbs}}</td>
+          </th>
+          <th>
+            <td>{{car.Acceleration}}</td>
+          </th>
+          <th>
+            <td>{{car.Year}}</td>
+          </th>
+          <th>
+            <td>{{car.Origin}}</td>
+          </th>
+        </tr> 
+         <tr  v-for="car in carsJson4" v-bind:key="car">
+          <th> 
+            <td>{{car.Name}}</td>
+          </th>
+          <th>
+            <td>{{car.Miles_per_Gallon}}</td>
+          </th>
+          <th>
+            <td>{{car.Cylinders}}</td>
+          </th>
+          <th>
+            <td>{{car.Displacement}}</td>
+
+          </th>
+          <th>
+            <td>{{car.Horsepower}}</td>
+          </th>
+          <th>
+             <td>{{car.Weight_in_lbs}}</td>
+          </th>
+          <th>
+            <td>{{car.Acceleration}}</td>
+          </th>
+          <th>
+            <td>{{car.Year}}</td>
+          </th>
+          <th>
+            <td>{{car.Origin}}</td>
+          </th>
+        </tr>  
+         <tr  v-for="car in carsJson5" v-bind:key="car">
+          <th> 
+            <td>{{car.Name}}</td>
+          </th>
+          <th>
+            <td>{{car.Miles_per_Gallon}}</td>
+          </th>
+          <th>
+            <td>{{car.Cylinders}}</td>
+          </th>
+          <th>
+            <td>{{car.Displacement}}</td>
+
+          </th>
+          <th>
+            <td>{{car.Horsepower}}</td>
+          </th>
+          <th>
+             <td>{{car.Weight_in_lbs}}</td>
+          </th>
+          <th>
+            <td>{{car.Acceleration}}</td>
+          </th>
+          <th>
+            <td>{{car.Year}}</td>
+          </th>
+          <th>
+            <td>{{car.Origin}}</td>
+          </th>
+        </tr>  
+       
+      </table>       --->
+    
   </div>
 </template>
 
 <script>
+import cars1 from '../data/cars-1.json'
+import cars2 from '../data/cars-2.json'
+import cars3 from '../data/cars-3.json'
+import cars4 from '../data/cars-4.json'
+import cars5 from '../data/cars-5.json'
+import Vuetify from 'vuetify'
+import Vue from 'vue'
+import 'vuetify/dist/vuetify.min.css'
+import lod from 'lodash'
+Vue.use(Vuetify)
+const allCars =  lod.concat(cars1, cars2, cars3, cars4, cars5)
+
+
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  //component: getAllCars,
+  data(){
+    return{
+    allCars,  
+      
+     
+      tableItems: [
+        {
+          text: 'Name',
+          align: 'start',
+          value: 'Name'
+        },
+        { text: "Miles_per_Gallon", value: "Miles_per_Gallon"},
+        { text: "Cylinders", value: "Cylinders"},
+        { text: "Displacement", value: "Displacement"},
+        { text: "Horsepower", value: "Horsepower"},
+        { text: "Weight_in_lbs", value: "Weight_in_lbs"},
+        { text: "Acceleration", value: "Acceleration"},
+        { text: "Year", value: "Year"},
+        { text: "Origin", value: "Origin"}
+      ]
+    }
+  },
+  mounted: function(){
+    this.getAllCars
+  },
+  method:{  
+  getAllCars: async function (){
+  let res = await fetch(allCars);
+  let cars = await res.json
+  return this.allCars = cars
+}  
+    
   }
+  
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border:  #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
 h3 {
   margin: 40px 0 0;
 }
